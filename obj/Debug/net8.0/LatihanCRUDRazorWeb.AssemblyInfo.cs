221b5e8c5318a056618caf519425f15328da4e7c
ButterMellown <dwae424@gmail.com>
2024-01-03 01:24:52 +0700

    done

--- obj/Debug/net8.0/LatihanCRUDRazorWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/LatihanCRUDRazorWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LatihanCRUDRazorWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115361d972076f8343f0f8513f59d2460228a306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LatihanCRUDRazorWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LatihanCRUDRazorWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
